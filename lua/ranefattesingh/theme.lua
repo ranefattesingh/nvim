0b32195cc9598c71153dde89474a1a8061a1b720
Sahil Rane Sardesai <41385129+ranefattesingh@users.noreply.github.com>
2024-01-02 17:20:39 +0530

    feat: integrated treesitter and syntax highlighting (#6)

---
+++ lua/ranefattesingh/theme.lua
@@ -0,0 +1,16 @@
+function ColorMyPencils(color)
+	color = color or 'onedark'
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+
+require('onedark').setup {
+    style = 'darker'
+}
+require('onedark').load()
+
+
+ColorMyPencils()
