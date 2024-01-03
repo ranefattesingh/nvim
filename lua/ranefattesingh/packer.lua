cddbac2870065b8f1ba2fff09e4a9af723b12e55
Sahil Rane Sardesai <41385129+ranefattesingh@users.noreply.github.com>
2024-01-03 11:33:44 +0530

    feat/zenmode (#13)

--- lua/ranefattesingh/packer.lua
+++ lua/ranefattesingh/packer.lua
@@ -55,4 +55,13 @@ return require('packer').startup(function(use)
             {"nvim-treesitter/nvim-treesitter"}
         }
     }
+
+    use{
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    }
 end)
